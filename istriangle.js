b7d524ebb49869e408bdb039eafea5e49e8961f2
Jangalanagababu42 <60348426+Jangalanagababu42@users.noreply.github.com>
2022-09-15 23:49:34 +0530

    Feat: Added Quiz

--- istriangle.js
+++ istriangle.js
@@ -1,6 +1,6 @@
 const inputs=document.querySelectorAll('.angle-input');
 const isTriangleBtn = document.querySelector('#is-triangle-btn');
-const outputE1=document.querySelector('#output');
+const outputEl=document.querySelector('#output');
 
 
 function calculateSumOfAngles(angle1, angle2, angle3){
@@ -13,10 +13,10 @@ function calculateSumOfAngles(angle1, angle2, angle3){
 function isTriangle(){
 const sumOfAngles=calculateSumOfAngles(Number(inputs[0].value),Number(inputs[1].value),Number(inputs[2].value));
 if(sumOfAngles===180){
-    outputE1.innerText="Yay, The angles form a Triangle";
+    outputEl.innerText="Yay, The angles form a Triangle";
 }
 else{
-    outputE1.innerText="ohh ohh, The angles NOT form a Triangle";  
+    outputEl.innerText="ohh ohh, The angles NOT form a Triangle";  
 }
 }
 isTriangleBtn.addEventListener("click",isTriangle);
---
+++ quiz.js
@@ -0,0 +1,17 @@
+const quizForm=document.querySelector('.quiz-form')
+const submitAnswerBtn=document.querySelector("#submit-answer-btn")
+const outputEl=document.querySelector("#output");
+const correctAnswers=[" Equilateral","√3/4 a2","3","10 cm","108 sq.cm"," 30 cm", "18 cm","4.33 cm"]
+function calculateScore(){
+    let score=0;
+    let index=0;
+    const formResults=new FormData(quizForm);
+    for(let value of formResults.values()){
+        if(value===correctAnswers[index]){
+            score=score+1;
+        }
+        index=index+1;
+    }
+    outputEl.innerText="Your Score is "+ score;
+}
+submitAnswerBtn.addEventListener('click',calculateScore)

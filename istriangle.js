4a692fcdcee5fbb3cf4cb7e8118092095a2a234c
Jangalanagababu42 <60348426+Jangalanagababu42@users.noreply.github.com>
2022-09-16 19:13:30 +0530

    fix: added negative scenerio

--- istriangle.js
+++ istriangle.js
@@ -11,12 +11,19 @@ function calculateSumOfAngles(angle1, angle2, angle3){
 
 
 function isTriangle(){
+      if (Number(inputs[0].value) > 0 && Number(inputs[1].value) > 0 && Number(inputs[2].value) > 0) {
+
 const sumOfAngles=calculateSumOfAngles(Number(inputs[0].value),Number(inputs[1].value),Number(inputs[2].value));
 if(sumOfAngles===180){
     outputEl.innerText="Yay, The angles form a Triangle";
 }
 else{
     outputEl.innerText="ohh ohh, The angles NOT form a Triangle";  
-}
+}}
+  else if (Number(inputs[0].value) === 0 && Number(inputs[1].value) === 0 && Number(inputs[2].value) === 0) {
+        outputEl.innerText="Please provide inputs/ Dont give all Zeroes";
+    } else {
+       outputEl.innerText="Can't accept negative values";
+    }
 }
 isTriangleBtn.addEventListener("click",isTriangle);

985e42471a725121cb610723be2df1971ce36bd2
Jangalanagababu42 <60348426+Jangalanagababu42@users.noreply.github.com>
2022-09-16 19:08:59 +0530

    fix:added edge cases

--- areaOfTriangles.js
+++ areaOfTriangles.js
@@ -8,9 +8,13 @@ function calculateArea(a,b){
 }
 
 function calculateareaOfTriangles(){
+    if (Number(sides[0].value) > 0 && Number(sides[1].value) > 0) {
     const areaOfTraingle=calculateArea(Number(sides[0].value),Number(sides[1].value));
     
-    outputEl.innerText="The Area of Triangle is " + areaOfTraingle
+    outputEl.innerText="The Area of Triangle is " + areaOfTraingle}
+    else{
+      outputEl.innerText="Don't be Silly Please Provide Some Valid Inputs😀😀😀 " }   
+    }
 }
 
 hypotenuseBtn.addEventListener("click",calculateareaOfTriangles);

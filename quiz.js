9609f58c58c2d56696c007e57067d50af9fb2d7a
Jangalanagababu42 <60348426+Jangalanagababu42@users.noreply.github.com>
2022-09-16 19:19:13 +0530

    fix: Handled negative

--- quiz.js
+++ quiz.js
@@ -5,13 +5,20 @@ const correctAnswers=["Equilateral","√3/4 a2","3","10 cm"," 108 sq.cm.","30 cm
 function calculateScore(){
     let score=0;
     let index=0;
+    int count=0;
     const formResults=new FormData(quizForm);
     for(let value of formResults.values()){
         if(value===correctAnswers[index]){
             score=score+1;
         }
         index=index+1;
+                count = count + 1;
+
     }
-    outputEl.innerText="Your Score is "+ score;
+    if(count===10){
+    outputEl.innerText="Your Score is "+ score;}
+    else{
+    outputEl.innerText="Please Provide all the Inputs ";
+    }
 }
 submitAnswerBtn.addEventListener('click',calculateScore)
